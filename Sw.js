58fc7a5ca188d2fb7094ac0a189f5538bc376e2a
alhmdmr291-bit <alhmdmr291@gmail.com>
2026-09-03 00:47:31 +0300

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,1 @@
+const CACHE='zenha-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./customer/','./customer/index.html','./admin/','./admin/index.html','./manifest.json','./icon-192.svg']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
