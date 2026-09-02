d82a5707e0134b8d6f0306c13c9bd791f1b31449
alhmdmr291-bit <alhmdmr291@gmail.com>
2026-09-02 16:20:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,24 @@
-const CACHE_NAME = "ZENHA-v1";
+const CACHE_NAME = "ZENHA-v2";
 
-const FILES_TO_CACHE = [
-  "/ZENHA/",
-  "/ZENHA/index.html",
-  "/ZENHA/manifest.json",
-  "/ZENHA/icon-192.png",
-  "/ZENHA/icon-512.png"
-];
-
-// تثبيت Service Worker
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-// تشغيل Service Worker
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
   event.waitUntil(self.clients.claim());
 });
 
-// جلب الملفات من الكاش عند الحاجة
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(fetchResponse => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    }).catch(() => {
+      return caches.match("/ZENHA/index.html");
     })
   );
 });
